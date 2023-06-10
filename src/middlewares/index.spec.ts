9100bdf1c3d11c50b21d70c3c0957a9e590b5303
junior m. soares <jrmsrs2.0@gmail.com>
2023-06-10 17:27:56 -0300

    🧪 (logger) coverage skip(req)

--- src/middlewares/index.spec.ts
+++ src/middlewares/index.spec.ts
@@ -1,7 +1,18 @@
 import Middlewares from './index'
+import { skip } from './logger'
+import { Request } from 'express'
 
 describe('Middlewares', () => {
   it('should ensure that all middlewares are called', () => {
     expect(Middlewares({ use: jest.fn() } as any)).toBeUndefined() // void
+  })
+
+  it('should ensure that logger skip it\'s working correctly', () => {
+    const reqFile: Request = { path: '/example.png' } as any
+    const reqNonFile: Request = { path: '/example' } as any
+    const skipResult1 = skip(reqFile)
+    const skipResult2 = skip(reqNonFile)
+    expect(skipResult1).toBe(true)
+    expect(skipResult2).toBe(false)
   })
 })
--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -3,6 +3,6 @@ import morgan from 'morgan'
 
 const toIgnore = /(css|js|png|jpg|jpeg|svg|ico|woff|woff2|ttf|eot|map)/
 
-export const loggerMiddleware = morgan('dev', {
-  skip: (req: Request) => toIgnore.exec(req.path) !== null
-})
+export const skip = (req: Request): boolean => toIgnore.exec(req.path) !== null
+
+export const loggerMiddleware = morgan('dev', { skip })
