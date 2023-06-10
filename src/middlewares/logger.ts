ad665b849b23154e0f304f4ed899916244aad727
junior m. soares <jrmsrs2.0@gmail.com>
2023-06-09 22:24:16 -0300

    🔧 (logger) skip logging files

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,3 +1,8 @@
+import { Request } from 'express'
 import morgan from 'morgan'
 
-export const loggerMiddleware = morgan('dev')
+const toIgnore = /(css|js|png|jpg|jpeg|svg|ico|woff|woff2|ttf|eot|map)/
+
+export const loggerMiddleware = morgan('dev', {
+  skip: (req: Request) => toIgnore.exec(req.path) !== null
+})
