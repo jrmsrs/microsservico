3c537fefe690020667d93a73e4bfbbbe964bab41
junior m. soares <jrmsrs2.0@gmail.com>
2023-07-07 02:27:00 -0300

    🔧 (jest) integration tests setup

--- jest.config.integration.cjs
+++ jest.config.integration.cjs
@@ -1,4 +1,5 @@
 const config = require('./jest.config.cjs')
 config.testRegex = '.*\\.test\\.ts$'
 config.coverageDirectory = 'coverage/integration'
+config.moduleNameMapper = {}
 module.exports = config
